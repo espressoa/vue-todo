2e76ee1c7214366b53583d93eb3fabcd78161f3a
espressoa <puredream27@gmail.com>
2022-10-17 22:21:37 +0900

    컴포넌트 생성 및 정의
    * localStorage에 데이터 넣는 부분까지 함

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <input type="text" v-model="newTodoItem"> <!-- input에 등록된 텍스트 값을 동적으로 -->
+      <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
